03c6321f00615b83b486d456731c8524e16cfe83
Ruben Romao <rubenromao@gmail.com>
2024-07-23 19:50:46 +0100

    code inspection and some changes

--- ViewModel/FeaturedProductData.php
+++ ViewModel/FeaturedProductData.php
@@ -3,24 +3,18 @@ declare(strict_types=1);
 
 namespace RubenRomao\DisplayFeaturedProduct\ViewModel;
 
-use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\Data\ProductSearchResultsInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\Product\Media\Config;
-use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollection;
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Catalog\Helper\Image;
 use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Store\Model\ScopeInterface;
 use Magento\Store\Model\StoreManagerInterface;
 use Psr\Log\LoggerInterface;
 use RubenRomao\DisplayFeaturedProduct\Block\FeaturedProductBlock;
-use RubenRomao\DisplayFeaturedProduct\Model\Config\Source\FeaturedSkusToOptionArray;
 
 /**
  * View Model to get the featured product data to be used in the block.
@@ -31,20 +25,16 @@ class FeaturedProductData implements ArgumentInterface
      * FeaturedProductData constructor.
      *
      * @param ProductRepositoryInterface $productRepository
-     * @param SearchCriteriaBuilder $searchCriteriaBuilder
-     * @param ProductCollection $productCollection
+     * @param SearchCriteriaBuilder $searchCriteria
      * @param Config $productMediaConfig
-     * @param Image $imageHelper
      * @param ScopeConfigInterface $scopeConfig
      * @param StoreManagerInterface $storeManager
      * @param LoggerInterface $logger
      */
     public function __construct(
         private readonly ProductRepositoryInterface $productRepository,
-        private readonly SearchCriteriaBuilder $searchCriteriaBuilder,
-        private readonly ProductCollection $productCollection,
+        private readonly SearchCriteriaBuilder $searchCriteria,
         private readonly Config $productMediaConfig,
-        private readonly Image $imageHelper,
         private readonly ScopeConfigInterface $scopeConfig,
         private readonly StoreManagerInterface $storeManager,
         private readonly LoggerInterface $logger,
@@ -72,14 +62,11 @@ class FeaturedProductData implements ArgumentInterface
 
         // Get the featured product if it is enabled and the SKU is set.
         if ($featuredEnabled && $featuredSku) {
-
-            $searchCriteria = $this->searchCriteriaBuilder
+            $searchProduct = $this->searchCriteria
                 ->addFilter('sku', $featuredSku, 'eq')
                 ->addFilter('is_featured', 1, 'eq')
                 ->create();
-
-            return $this->productRepository->getList($searchCriteria);
-
+            return $this->productRepository->getList($searchProduct);
         } else {
             $this->logger->error('Featured product is not enabled or SKU is not set.');
             return null;
@@ -87,20 +74,16 @@ class FeaturedProductData implements ArgumentInterface
     }
 
     /**
-     * Get the featured product image.
+     * Get the featured product URL image by SKU.
      *
-     * @param $productSku
+     * @param string $productSku
      * @return string
      * @throws NoSuchEntityException
      */
-    Public function getProductImageUrl($productSku): string
+    public function getProductImageUrl(string $productSku): string
     {
-        $store = $this->storeManager->getStore();
-
         $product = $this->productRepository->get($productSku);
-
-        //$productUrl = $product->getProductUrl();
-        //$productU = $product->getProductUrl();
+        $store = $this->storeManager->getStore();
 
         return $store->getBaseUrl(UrlInterface::URL_TYPE_MEDIA) .
                   $this->productMediaConfig->getBaseMediaPath() .
@@ -108,16 +91,15 @@ class FeaturedProductData implements ArgumentInterface
     }
 
     /**
-     * Get the featured product URL.
+     * Get the product detail page URL by SKU.
      *
-     * @param $productSku
+     * @param string $productSku
      * @return string
      * @throws NoSuchEntityException
      */
-    public function productUrl($productSku): string
+    public function getProductDetailPageUrl(string $productSku): string
     {
         $product = $this->productRepository->get($productSku);
-
         return $product->getProductUrl();
     }
 }
